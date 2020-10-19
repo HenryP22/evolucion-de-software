848e00e0359e71a7700d0e5564f4ca2d16c72b5d
w7moises <w7moises@gmail.com>
2020-10-18 22:02:18 -0500

    tarjeta padre feature and step

---
+++ AcceptanceTests/Features/Tarjeta.feature
@@ -0,0 +1,23 @@
+﻿Feature: Tarjeta
+	Registrar tarjeta
+
+@mytag
+Scenario: Ingresar tarjeta
+	Given el padre quiere registar su tarjeta
+	When este en la opcion mi perfil
+	Then se mostrara una opcion mi tarjeta
+	And seleccionara agregar tarjeta
+	And registrara el numero y fecha de vencimiento
+
+@mytag
+Scenario: Eliminar tarjeta
+	Given el padre quiere eliminar su tarjeta
+	When este en la opcion mi perfil
+	Then se mostrara una opcion mi tarjeta
+	And eliminara la tarjeta mostrada mediante un boton
+
+@mytag
+Scenario: Datos mal ingresados
+	Given el sistema rechazo la tarjeta ingresada
+	When el padre completo los datos de la tarjeta
+	Then se mostrara un mensaje de datos incorrectos
---
+++ AcceptanceTests/Steps/TarjetaSteps.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests.Steps
+{
+    [Binding]
+    public sealed class TarjetaSteps
+    {
+        [Given(@"el padre quiere registar su tarjeta")]
+        public void GivenElPadreQuiereRegistarSuTarjeta()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"este en la opcion mi perfil")]
+        public void WhenEsteEnLaOpcionMiPerfil()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"se mostrara una opcion mi tarjeta")]
+        public void ThenSeMostraraUnaOpcionMiTarjeta()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"seleccionara agregar tarjeta")]
+        public void ThenSeleccionaraAgregarTarjeta()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"registrara el numero y fecha de vencimiento")]
+        public void ThenRegistraraElNumeroYFechaDeVencimiento()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"el padre quiere eliminar su tarjeta")]
+        public void GivenElPadreQuiereEliminarSuTarjeta()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"eliminara la tarjeta mostrada mediante un boton")]
+        public void ThenEliminaraLaTarjetaMostradaMedianteUnBoton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"el sistema rechazo la tarjeta ingresada")]
+        public void GivenElSistemaRechazoLaTarjetaIngresada()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"el padre completo los datos de la tarjeta")]
+        public void WhenElPadreCompletoLosDatosDeLaTarjeta()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"se mostrara un mensaje de datos incorrectos")]
+        public void ThenSeMostraraUnMensajeDeDatosIncorrectos()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
