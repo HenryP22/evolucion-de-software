3b4b1feac12e980c6bb7bd7c0052a7e7c02be748
w7moises <w7moises@gmail.com>
2020-10-18 22:00:59 -0500

    informe padre feature and step

---
+++ AcceptanceTests/Features/Informe.feature
@@ -0,0 +1,22 @@
+﻿Feature: Informe
+	Revisar informe
+
+@mytag
+Scenario: Informe no subido
+	Given el padre esta en la seccion clases
+	When se dirija a detalles
+	Then aparecera un mensaje de no subido
+
+@mytag
+Scenario: Informe subido
+	Given el padre esta en la seccion clases
+	When se dirija a detalles
+	Then podra visualizar el informe
+	And podra descargarlo mediante un boton
+
+@mytag
+Scenario: Informe incorrecto
+	Given el padre esta en la seccion clases
+	When se dirija a detalles
+	And revise el archivo
+	Then podra enviar una notificacion si es incorrecto
---
+++ AcceptanceTests/Steps/InformeSteps.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests.Steps
+{
+    [Binding]
+    public sealed class InformeSteps
+    {
+        [Given(@"el padre esta en la seccion clases")]
+        public void GivenElPadreEstaEnLaSeccionClases()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"se dirija a detalles")]
+        public void WhenSeDirijaADetalles()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"aparecera un mensaje de no subido")]
+        public void ThenApareceraUnMensajeDeNoSubido()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"podra visualizar el informe")]
+        public void ThenPodraVisualizarElInforme()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"podra descargarlo mediante un boton")]
+        public void ThenPodraDescargarloMedianteUnBoton()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"revise el archivo")]
+        public void WhenReviseElArchivo()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"podra enviar una notificacion si es incorrecto")]
+        public void ThenPodraEnviarUnaNotificacionSiEsIncorrecto()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
