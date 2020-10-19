61e7a6b68d277a74536671bb56c53ba94a802625
w7moises <w7moises@gmail.com>
2020-10-18 21:58:05 -0500

    perfil feature and step

---
+++ AcceptanceTests/Features/Perfil.feature
@@ -0,0 +1,20 @@
+﻿Feature: Perfil
+	Ver perfil del usuario
+
+@mytag
+Scenario: Ingresar a mi perfil
+	Given el usuario quiera verificar su perfil
+	When presione la opcion mi perfil
+	Then podra revisar los datos del perfil mostrado
+
+@mytag
+Scenario: Visualizar datos personales
+	Given el usuario esta en mi perfil
+	When quiera acceder a la información
+	Then se mostrara sus datos
+
+@mytag
+Scenario: Registro de actividades 
+	Given el usuario quiere ver sus recientes actividades
+	When ingrese a mi perfil
+	Then se mostrara una lista de actividades
---
+++ AcceptanceTests/Steps/PerfilSteps.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using TechTalk.SpecFlow;
+
+namespace AcceptanceTests.Steps
+{
+    [Binding]
+    public sealed class PerfilSteps
+    {
+        [Given(@"el usuario quiera verificar su perfil")]
+        public void GivenElUsuarioQuieraVerificarSuPerfil()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"presione la opcion mi perfil")]
+        public void WhenPresioneLaOpcionMiPerfil()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"podra revisar los datos del perfil mostrado")]
+        public void ThenPodraRevisarLosDatosDelPerfilMostrado()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"el usuario esta en mi perfil")]
+        public void GivenElUsuarioEstaEnMiPerfil()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"quiera acceder a la información")]
+        public void WhenQuieraAccederALaInformacion()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"se mostrara sus datos")]
+        public void ThenSeMostraraSusDatos()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Given(@"el usuario quiere ver sus recientes actividades")]
+        public void GivenElUsuarioQuiereVerSusRecientesActividades()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [When(@"ingrese a mi perfil")]
+        public void WhenIngreseAMiPerfil()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+        [Then(@"se mostrara una lista de actividades")]
+        public void ThenSeMostraraUnaListaDeActividades()
+        {
+            ScenarioContext.Current.Pending();
+        }
+
+    }
+}
